af06cdf73ebb2e37e3915e5167e7bda97562e046
harshil lakhan <harshillakhani009.com>
2023-08-17 16:24:31 +0530

    blog-project

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,65 +1,57 @@
+document.querySelector("#upform").addEventListener("submit", (e) => {
+  e.preventDefault();
 
-document.querySelector("#upform").addEventListener("submit",(e)=>{
-    e.preventDefault()
+  updata = {
+    username: document.querySelector("#username").value,
+    email: document.querySelector("#email").value,
+    password: document.querySelector("#password").value,
+  };
+  // let username = document.querySelector("#username").value;
+  // let email = document.querySelector("#email").value;
+  // let password = document.querySelector("#password").value;
 
-    let username = document.querySelector("#username").value;
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
+  let userval = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+  let emailval = /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let passval = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
 
-    let userval = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
-    let emailval = /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    let passval = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+  if (!userval.test(updata.username)) {
+    document.querySelector("#uerror").innerHTML =
+      "Please enter valid username ";
+  }
+  if (!emailval.test(updata.email)) {
+    document.querySelector("#eerror").innerHTML =
+      "Please enter valid Email address";
+  }
+  if (!passval.test(updata.password)) {
+    document.querySelector("#perror").innerHTML = "Enter minimum 8 latter ";
+  }
 
-    if(userval.test(username)){
-        document.querySelector("#uerror").innerHTML=""
-    }else{
-        document.querySelector("#uerror").innerHTML="Please enter valid username "
-    }
-    if(emailval.test(email)){
-        document.querySelector("#eerror").innerHTML=""
-    }else{
-        document.querySelector("#eerror").innerHTML="Please enter valid Email address"
-    }
-    if(passval.test(password)){
-        document.querySelector("#perror").innerHTML=""
-    }else{
-        document.querySelector("#perror").innerHTML="Enter minimum 8 latter "
-    }
-    
-        if(userval.test(username) && emailval.test(email) && passval.test(password)){
-            window.location.href="index.html"
+  if (
+    userval.test(updata.username) &&
+    emailval.test(updata.email) &&
+    passval.test(updata.password)
+  ) {
+    fetch(`http://localhost:3100/signup?email=${updata.email}`)
+      .then((ele) => ele.json())
+      .then((mix) => {
+        if (mix.length > 0) {
+
+          document.querySelector("#ferror").innerHTML = "* Email is already registered ";
+          setTimeout(() => {
+                window.location.href = "/html/signin.html";
+            }, 3000);
+        } else {
+
+            fetch("http://localhost:3100/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(updata),
+            });
+            alert("login")
+            setTimeout(() => {
+                window.location.href = "/html/index.html";
+            }, 3000);
         }
-
-    updata={
-        username : document.querySelector("#username").value,
-        email : document.querySelector("#email").value,
-        password : document.querySelector("#password").value
-    }
-
-    fetch(`http://localhost:3100/signup?email=${email}`)
-    .then((ele)=>ele.json())
-    .then((mix)=>{
-        if(mix.length > 0){
-            for(let i = 0; i < mix.length ; i++){
-                if(mix[i].email == email){
-                    document.querySelector("#ferror").innerHTML="* Email is already registered "
-                    alert("email is already ragistrate")
-                }
-                else{
-                    window.location.href="index.html"
-                }
-            }
-        }
-        else{
-            document.querySelector("#ferror").innerHTML="fill data"
-        }
-    })
-
-    fetch("http://localhost:3100/signup",{
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(updata)
-    })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-})  
+      });
+  }
+});
